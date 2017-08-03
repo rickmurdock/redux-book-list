91c5b0281eabd9b987f23e51163ae9d683953433
rickmurdock <rick.murdock@live.com>
2017-08-03 09:01:30 -0500

    Completed activity code.

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,14 @@ import BookDetail from '../containers/BookDetail';
 class App extends Component {
     render() {
         return (
-            <div className="row">
-                <h1>Books N' Roses: Appetite for Instruction</h1>
-                <BookList/>
-                <BookDetail/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2">
+                        <h1>Books N' Roses: Appetite for Instruction</h1>
+                        <BookList/>
+                        <BookDetail/>
+                    </div>
+                </div>
             </div>
         );
     }
